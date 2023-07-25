6d0356af6d8261f3d92649327a9705eb9efd8ece
MaheshaRathnayaka <mahesharathnayaka20@gmail.com>
2023-07-25 14:14:59 -0700

    firstdemocalculator

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import classes from "./Button.module.css"
+
+function Button({value, handler}) {
+  return (
+    //making condition
+    <div className={value == "C" || value == "=" ? `${classes.calcBtns} ${classes.res}` : `${classes.calcBtns}`} 
+    onClick={() => handler(value)}>
+
+        {value}
+    </div>
+  )
+}
+
+export default Button
